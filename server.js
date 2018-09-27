4d384747c0188053f0022b92632b5fb35d951df8
simmi9 <bhawna.keswani20@gmail.com>
2018-09-27 16:34:29 -0400

    importing bodyParser for JSON and mongoose for mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 var text = "hello";
 
