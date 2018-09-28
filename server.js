58977b644285698e71c73f30e35cacfafca3f3e7
simmi9 <bhawna.keswani20@gmail.com>
2018-09-27 22:20:02 -0400

    Created POST request for adding product in Product schema in Mongodb

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/swag-shop');
+var db = mongoose.connect('mongodb://localhost/shop');
 
 var Product = require('./model/product');
 var WishList = require('./model/wishlist');
@@ -15,6 +15,19 @@ var text = "hello";
 app.get('/', function(request, response) {
 	response.send(text);
 	});
+
+app.post('/product', function(request, response) {
+    var product = new Product();
+    product.title = request.body.title;
+    product.price = request.body.price;
+    product.save(function(err, savedProduct) {
+       if (err) {
+           response.status(500).send({error:"Could not save product"});
+       } else {
+           response.send(savedProduct);
+       }
+    });
+});
 
 app.listen(3007, function() {
     console.log("Shop API running on port 3007...");
